a3f98d54c92a00769bbd39b3f2017756957a75de
aidb1993 <53492034+aidb1993@users.noreply.github.com>
2019-11-26 18:09:35 -0300

    Add files via upload

--- js/nav__toggler.js
+++ js/nav__toggler.js
@@ -1,6 +1,7 @@
 const toggler = document.querySelector('.toggle')
 const navLinks = document.querySelector('.nav__links')
-
+const main = document.querySelector('.main')
 toggler.addEventListener("click", () => {
     navLinks.classList.toggle('active')
+    main.classList.toggle('active')
 });
