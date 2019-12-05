96fb3f2720299962c17b1f4a66f5fb29882771bc
aidb1993 <53492034+aidb1993@users.noreply.github.com>
2019-12-05 17:52:30 -0300

    Add files via upload

--- js/tips__toggler.js
+++ js/tips__toggler.js
@@ -2,15 +2,25 @@ const drawer_toggler = document.querySelectorAll('.drawer-toggler');
 const drawer_content = document.querySelectorAll('.drawer__content');
 const drawer_active = document.querySelector('.drawer__content--active');
 let togglerArray = Array.from(drawer_toggler);
+let drawerArray = Array.from(drawer_content);
 
+function drawerReset() {
+    drawerArray.forEach(element => {
+        element.classList.remove('drawer__content--active')
+    });
+}
 
 function setDrawer(drawerI) {
+    drawerReset();
     drawer_content[drawerI].classList.toggle('drawer__content--active')
 }
+
+
 function enable () {
         togglerArray.forEach(element => {
         element.addEventListener('click' , () => {
             const drawerI = togglerArray.indexOf(element);
+            drawerIndex = drawerI;
             setDrawer(drawerI);
         })
     });
