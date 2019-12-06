fefc10f08d24c5d8732be2b20e0f7e1cec4da360
aidb1993 <53492034+aidb1993@users.noreply.github.com>
2019-12-06 17:44:07 -0300

    Add files via upload

---
+++ js/hero-slider.js
@@ -0,0 +1,24 @@
+const hero = document.querySelector('.hero');
+let heroIndex = 0;
+
+function heroReset() {
+    hero.classList.remove(`hero-slider-${heroIndex}`)
+}
+
+function heroChanger() {
+    hero.classList.add(`hero-slider-${heroIndex}`)
+}
+
+// Currently setted for 2 slides in case of more simply change if(heroIndex<X)
+setInterval(() => {
+    if(heroIndex<1) {
+        heroReset()
+        heroIndex++
+        heroChanger();
+    } else {
+        heroReset()
+        heroIndex = 0;
+        heroChanger();
+    }
+}, 6000);
+
