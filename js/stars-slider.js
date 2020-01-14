ff589a60629d9f495fa7694dfa93ebf7c07ca7a1
aidb1993 <53492034+aidb1993@users.noreply.github.com>
2020-01-14 14:52:38 -0300

    Add files via upload

--- js/stars-slider.js
+++ js/stars-slider.js
@@ -1,53 +1,34 @@
-// const starsLeft = document.querySelector('#stars-content-left');
-// const starsRight = document.querySelector('#stars-content-right');
-// const starsCards = document.querySelectorAll('.stars-content-card');
-// let starsCardsIndex = 0;
-
-// // function starsCardsReset() {
-// //     starsCards.forEach(element => {
-// //         element.classList.remove('content-card-disabled')
-
-// //     });
-// // }
-
-// function starsCardsArrowsSetter() {
-//     const newArray = Array.from(starsCards)
-//     starsRight.addEventListener('click' ,() => {
-       
-//     })
-
-//     starsLeft.addEventListener('click', () =>{
-//         if(starsCardsIndex !=0) {
-//             starsCardsIndex--
-//         } else {
-//             starsCardsIndex = (starsCards.length - 1)
-//         }
-//         console.log(starsCardsIndex)
-//     })
-    
-// }
+const starsLeft = document.querySelector('#stars-content-left');
+const starsRight = document.querySelector('#stars-content-right');
+const starsCards = document.querySelectorAll('.stars-content-card');
+let starsCardsIndex = 1;
+let starsCardsMargin = 0;
 
 
+function starsArrowsSetter() {
+  starsLeft.addEventListener('click', () => {
+    if(starsCardsIndex < starsCards.length - 1){
+    starsCardsMargin = starsCardsMargin - 384;
+    starsCards[0].style.marginLeft = `${starsCardsMargin}px`;
+    starsCardsIndex++;
+    } else {
+      return
+    }
+  })
+
+  starsRight.addEventListener('click', () => {
+    if ( starsCardsIndex != 0) {
+    starsCardsMargin = starsCardsMargin + 384;
+    starsCards[0].style.marginLeft = `${starsCardsMargin}px`;
+    starsCardsIndex--;
+    } else {
+      return
+    }
+  })
+}
+
+starsArrowsSetter()
 
 
-// function starsCardsInitial () {
-//     const newArray = Array.from(starsCards)
 
-//     newArray.forEach(element => {
-//         element.style.order = newArray.indexOf(element);
-//         console.log( element.style.order)
-//     });
-// }
 
-// starsCardsInitial();
-// starsCardsArrowsSetter();
-
-$(document).ready(() => {
-    $('.stars-slider').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: false,
-      arrows: true,
-      fade: true,
-    });
-  });
